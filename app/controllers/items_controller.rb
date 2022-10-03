cb2dede629d2e4d7b5f7f21c9dcb1288f451fdef
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-10-03 05:21:43 +0300

    commit tree 6bd0d69921f4ad46e7c964750ec1de24fed2fca9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
-class ItemsController < ApplicationController
+class ItemsController < ApplicationController 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
-    items = Item.all
-    render json: items, include: :user
+  def index 
+    if params[:user_id] 
+      user = User.find(params[:user_id]) 
+      items = user.items 
+    else
+    items = Item.all 
+    end
+    render json: items.to_json(except: [:created_at, :updated_at], include: :user)
   end
+
+  def show 
+    user = Item.find(params[:id]) 
+    render json: user
+  end  
+
+  def create 
+    item = Item.create(item_params) 
+   
+    render json: item, status: :created 
+   
+
+  end
+
+  private
+
+  def item_params 
+    params.permit(:name, :description, :price, :user_id)
+  end 
+
+  def render_not_found_response
+    render json: { error: "Dog house not found" }, status: :not_found
+  end
+
 
 end
